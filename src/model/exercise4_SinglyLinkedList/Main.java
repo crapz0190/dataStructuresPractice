1e6ec4664630b168da34f818511a567ac1bf8ebb
crapz <crapz0190@gmail.com>
2026-03-31 22:13:49 -0300

    feat: implement addPos method for positional insertion
    - Added addPos(E e, int n) to insert elements at a specific index. - Optimized performance by using addFirst and addLast for boundary cases. - Included index validation using checkIndex.

---
+++ src/model/exercise4_SinglyLinkedList/Main.java
@@ -0,0 +1,25 @@
+package model.exercise4_SinglyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        var sList = new SinglyLinkedList<Integer>();
+        Integer num1 = 34;
+        Integer num2 = 65;
+        Integer num3 = 12;
+        Integer num4 = 65;
+        sList.addFirst(num1);
+        sList.addFirst(num2);
+        sList.addLast(num3);
+        sList.addLast(num4);
+
+        try {
+            Integer num5 = 23;
+            int position = 2;
+            sList.addPos(num5, position);
+            System.out.println(sList);
+            System.out.println("Size of list: " + sList.size());
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
